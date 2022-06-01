1a8d2c6fcf50e50b158d5e57cc940b6663257420
Lennon Martins <lennonmcarlos@gmail.com>
2022-06-01 13:27:09 -0400

    Cálculo dos demais pacientes

--- JavaScript Web/introducao-javascript/js/index.js
+++ JavaScript Web/introducao-javascript/js/index.js
@@ -1,29 +1,37 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var peso = paciente.querySelector(".info-peso").textContent;
-var altura = paciente.querySelector(".info-altura").textContent;
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
 
-calculoImc = peso / (altura * altura);
 
-var imc = paciente.querySelector(".info-imc");
+for (let i = 0; i < pacientes.length; i++) {
+	var paciente = pacientes[i];
+	console.log(paciente);
 
-alturaEValida = true;
-pesoEValido = true;
+	var peso = paciente.querySelector(".info-peso").textContent;
+	var altura = paciente.querySelector(".info-altura").textContent;
 
-if (peso <= 0 || peso >= 1000) {
-	pesoEValido = false;
-	imc.textContent = "Peso inválido";
-}
+	calculoImc = peso / (altura * altura);
 
-if (altura <= 0 || altura >= 3.00) {
-	alturaEValida = false;
-	imc.textContent = "Altura inválida";
-}
+	var imc = paciente.querySelector(".info-imc");
 
-if (alturaEValida && pesoEValido) {
-	imc.textContent = calculoImc;
-} else{
-	imc.textContent = "Altura e/ou peso inválidos!";
+	alturaEValida = true;
+	pesoEValido = true;
+
+	if (peso <= 0 || peso >= 1000) {
+		pesoEValido = false;
+		imc.textContent = "Peso inválido";
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if (altura <= 0 || altura >= 3.00) {
+		alturaEValida = false;
+		imc.textContent = "Altura inválida";
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if (alturaEValida && pesoEValido) {
+		imc.textContent = calculoImc.toFixed(2);
+	}
 }
