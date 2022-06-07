b25a1070fa5a385e122cef748bd8bd0b8a9b3637
Lennon Martins <lennonmcarlos@gmail.com>
2022-06-07 17:28:50 -0400

    Não tirar TR

--- JavaScript Web/introducao-javascript/js/remover-pacientes.js
+++ JavaScript Web/introducao-javascript/js/remover-pacientes.js
@@ -4,12 +4,15 @@ var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function (evento) {
     var elementoAlvo = evento.target;
+
+    if(evento.target.tagName == "TD" ){
     var elementoPai = elementoAlvo.parentNode;
     elementoPai.classList.add("fadeOut");
 
     setTimeout(function () {
         elementoPai.remove();
     }, 700);
+};
 });
 
 
